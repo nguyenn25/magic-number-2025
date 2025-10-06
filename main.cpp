c3d5c6436d3beb7a621ce8240b5d47e49a0d7ca5
nguyenn25 <nguyenn25@spu.edu>
2025-10-06 13:46:24 -0700

    version 2 is done

--- main.cpp
+++ main.cpp
@@ -11,13 +11,19 @@ int main() {
     srand(time(0));
     int magic = rand() % 10 + 1; // methods <> functions
     int guess;
+    int life = 3;
     cout << "What is your guess: ";
-    cin >> guess;
-    if (magic == guess) { //assigns guess to magic
-        cout << "You are a winner!" << endl;
-    }
-    else {
-        cout << "Loser!" << endl;
+    while (life > 0) {
+        cin >> guess;
+        if (magic == guess) { //assigns guess to magic
+            cout << "You are a winner!" << endl;
+            return 0;
+        }
+        else {
+            cout << "Loser!" << endl;
+            life -= 1;
+            cout << "Guess again: ";
+        }
     }
     cout << magic;
     return 0;
